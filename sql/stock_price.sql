f19ba2bb91b577a01ec581ddbc063b6de99023bf
Jason-happy-ops <2941854208@qq.com>
2026-06-08 22:57:15 +0800

    renew README

--- sql/stock_price.sql
+++ sql/stock_price.sql
@@ -1,17 +1,19 @@
-Create TABLE stock_price
-(	stock_code VARCHAR(10),
- 	trade_date date,
- 	open_price DECIMAL(3,2),
- 	cloese_price DECIMAL(3,2),
- 	high_price DECIMAL(3,2),
- 	low_price DECIMAL(3,2),
- 	volume INT(20),
-	amount BIGINT,
- 	amplitude DECIMAL(3,2),
- 	pct_change DECIMAL(3,2),
- 	change_amount DECIMAL(3,2),
- 	
- 	PRIMARY KEY (stock_code,trade_date),
-	FOREIGN KEY (stock_code) REFERENCES stock_info(stock_code)
- 	ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE stock_price
+(
+    stock_code      VARCHAR(10),
+    trade_date      DATE,
+    open_price      DECIMAL(10, 2),
+    close_price     DECIMAL(10, 2),      
+    high_price      DECIMAL(10, 2),
+    low_price       DECIMAL(10, 2),
+    volume          INT(20),
+    amount          DECIMAL(15, 2),       
+    amplitude       DECIMAL(5, 2),
+    pct_change      DECIMAL(5, 2),
+    change_amount   DECIMAL(10, 2),
+    turnover        DECIMAL(5, 2),
+
+    PRIMARY KEY (stock_code, trade_date),
+    FOREIGN KEY (stock_code) REFERENCES stock_info(stock_code)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
